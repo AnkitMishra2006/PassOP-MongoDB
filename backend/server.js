f0716841ae9e1b3d08ac4f61266f01e7cf9e7758
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-04-19 07:39:32 +0530

    Basic express setup in backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const app = express();
 const port = 3000;
 
-const dotenv = require("dotenv");
 dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
