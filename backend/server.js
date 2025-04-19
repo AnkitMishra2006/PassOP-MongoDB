051bab88c36bfedeef0c4c6cfc0bccf6280d8341
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-04-19 08:23:42 +0530

    API Routes to get all passwords and insert one password done

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-const app = express();
-const port = 3000;
+const { MongoClient, Collection } = require("mongodb");
+const bodyParser = require("body-parser");
 
 dotenv.config();
+const url = process.env.MONGO_URI || "mongodb://localhost:27017";
+console.log(url);
+console.log(process.env.MONGO_URI);
+const client = new MongoClient(url);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const dbName = "passop";
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+
+client.connect();
+
+app.get("/", async (req, res) => {
+  const db = client.db(dbName);
+  console.log(db);
+  const collection = db.collection("passwords");
+  const findResult = await collection.find({}).toArray();
+
+  res.json(findResult);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.post("/", async (req, res) => {
+  try {
+    const password = req.body;
+
+    const db = client.db(dbName);
+    console.log(db);
+    const collection = db.collection("passwords");
+    const insertResult = await collection.insertOne(password);
+    res.json({
+      message: "Password saved successfully",
+      insertedId: insertResult.insertedId,
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port || 3000, () => {
+  console.log(`Server is running on port ${port || 3000}`);
 });
